ae6c7c09d3de95c2b268d7688403b9cf8b3bc485
jtfrench23 <92399356+jtfrench23@users.noreply.github.com>
2022-01-16 21:14:29 -0600

    added additional functionality and clear session route

--- server.py
+++ server.py
@@ -4,15 +4,34 @@ app.secret_key='8675309DMX'
 
 @app.route('/')         
 def index():
-    session['count']+=1
+    if 'visits' not in session:
+        session['visits']=1
+    else:
+        session['visits']+=1
+    if "count" not in session:
+        session["count"] = 1
+    else:
+        session['count'] += 1
     return render_template("index.html")
 @app.route('/count', methods=['POST'])
 def count():
     session['count']+=1
     return redirect('/')
+@app.route('/increase_amount',methods=['POST'])
+def increase_count():
+    amount=int(request.form['quantity'])
+    session['count']+=amount-1
+    return redirect('/')
+
 @app.route('/reset', methods=['POST'])
 def reset():
     session['count']=0
     return redirect('/')
+
+@app.route('/destroy_all')
+def destroy_all():
+    session.clear()
+    return redirect('/')
+
 if __name__=="__main__":   
     app.run(debug=True)    
